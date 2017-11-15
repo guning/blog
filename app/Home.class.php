4c05d2fc693d71f6b56d160b8c012082a13743c7
guning <2264978870@qq.com>
2017-11-16 00:11:41 +0800

    init project

---
+++ app/Home.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guning
+ * Date: 2017/11/10
+ * Time: 10:35
+ */
+
+namespace app;
+
+
+class Home
+{
+
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guning
+ * Date: 2017/11/10
+ * Time: 9:59
+ */
